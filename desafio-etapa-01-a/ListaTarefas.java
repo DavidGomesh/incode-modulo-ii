b3fb4567010a5f318bc8ea46322eaeff28075e11
David Gomesh <davidgomesh2016@gmail.com>
2024-09-03 09:31:48 -0300

    Removido acentos e adicionado opções de sair

--- desafio-etapa-01-a/ListaTarefas.java
+++ desafio-etapa-01-a/ListaTarefas.java
@@ -15,64 +15,89 @@ public class ListaTarefas {
         System.out.println("Banco de tarefas alimentado!\n\n");
         concluidas.addAll(Arrays.asList(false, false, false));
         tarefas.addAll(Arrays.asList(
-            "Estudar para prova de Cálculo",
-            "Tirar cópia da chave de casa",
+            "Estudar para prova de Calculo",
+            "Tirar copia da chave de casa",
             "Separar roupas velhas"
         ));
 
         int opcao = 0;
         do {
             System.out.println("\n\n\n\n\n");
-            System.out.println("===== MENU =====");
-            System.out.println("1 - Adicionar");
-            System.out.println("2 - Marcar como concluída");
-            System.out.println("3 - Listar");
+            System.out.println("========== MENU ==========");
+            System.out.println("1 - Adicionar tarefas");
+            System.out.println("2 - Marcar como concluida");
+            System.out.println("3 - Listar tarefas");
             System.out.println("0 - Sair");
-            
-            System.out.print("OPÇÃO: ");
-            opcao = scanner.nextInt();
+
+            System.out.print("OPCAO: ");
+            try {
+                opcao = scanner.nextInt();
+            } catch (Exception e) {
+                opcao = -1;
+            }
 
             System.out.println("\n\n\n\n\n");
             scanner.nextLine();
 
             switch (opcao) {
-                case 1: 
+                case 1:
                     System.out.println("----- ADICIONAR TAREFA -----");
 
+                    System.out.println("\n* Digite 0 para sair!");
                     System.out.print("Informe a tarefa: ");
-                    tarefas.add(scanner.nextLine());
+                    var tarefa = scanner.nextLine();
+
+                    if(tarefa.equals("0")) {
+                        break;
+                    }
+
+                    tarefas.add(tarefa);
                     concluidas.add(false);
-                    
+
                     System.out.println("\nTAREFA CADASTRADA!");
                 break;
-                    
+
                 case 2:
                     System.out.println("----- CONCLUIR TAREFA -----");
                     if (tarefas.isEmpty()) {
-                        System.out.println("Não há tarefas cadastradas!");
+                        System.out.println("Nao ha tarefas cadastradas!");
                         break;
                     }
-                    
-                    int numTarefa;
+
+                    for (int i=0; i<tarefas.size(); i++) {
+                        System.out.print("- " + (i+1) + (concluidas.get(i) ? " (x) " : " ( ) "));
+                        System.out.println(tarefas.get(i));
+                    }
+
+                    int numTarefa = -1;
                     do {
-                        System.out.print("Informe o número da tarefa: ");
-                        numTarefa = scanner.nextInt();
+                        System.out.println("\n* Digite 0 para sair!");
+                        System.out.print("Informe o numero da tarefa: ");
+                        try {
+                            numTarefa = scanner.nextInt();
+                        } catch (Exception e) {
+                            numTarefa = -1;
+                        }
                         scanner.nextLine();
 
                         if (numTarefa < 0 || numTarefa > tarefas.size()) {
-                            System.out.println("TAREFA INVÁLIDA!");
+                            System.out.println("TAREFA INVALIDA!");
                         }
 
                     } while(numTarefa < 0 || numTarefa > tarefas.size());
-                    
+
+                    if(numTarefa == 0) {
+                        break;
+                    }
+
                     concluidas.set(numTarefa-1, true);
-                    System.out.println("\nTAREFA '" + tarefas.get(numTarefa-1) + "' CONCLUÍDA!");
+                    System.out.println("\nTAREFA '" + tarefas.get(numTarefa-1) + "' CONCLUIDA!");
                 break;
 
-                case 3: 
+                case 3:
                     System.out.println("----- LISTA DE TAREFAS -----");
                     if (tarefas.isEmpty()) {
-                        System.out.println("Não há tarefas cadastradas!");
+                        System.out.println("Nao ha tarefas cadastradas!");
                         break;
                     }
 
@@ -84,12 +109,12 @@ public class ListaTarefas {
                     System.out.println("----------------------------");
                 break;
 
-                case 0: 
-                    System.out.println("PROGRAMA ENCERRADO!"); 
+                case 0:
+                    System.out.println("PROGRAMA ENCERRADO!");
                 break;
-            
+
                 default:
-                    System.out.println("OPÇÃO INVÁLIDA!");
+                    System.out.println("OPCAO INVALIDA!");
                 break;
             }
 
